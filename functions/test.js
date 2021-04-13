bf685fe926d069280f201f96ff7c8eb585d314c0
Miguel rojas <leprosy57@gmail.com>
2021-04-13 12:05:32 -0400

    Update test.js

--- functions/test.js
+++ functions/test.js
@@ -1,4 +1,7 @@
 exports.handler = async function(event, context) {
     // your server-side functionality
-  return "THIS IS TEST"
+    return {
+        statusCode: 200,
+        body: JSON.stringify({event, context})
+    };
 }
